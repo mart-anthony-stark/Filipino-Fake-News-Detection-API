9ba5b83350aef40bccb21ba6ae0bbc8c4af7cae2
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-05 10:49:42 +0800

    add run

--- web.py
+++ web.py
@@ -4,37 +4,40 @@ import numpy as np
 import pickle
 from urllib.request import urlopen
 
-app = Flask(__name__)
+def run():
+  app = Flask(__name__)
 
-# Load saved model pipeline
-clf_pipeline = pickle.load(urlopen('https://github.com/mart-anthony-stark/Machine-Learning-and-Deep-Learning/blob/main/ph_fake_news/ph_fake_news_pipeline.pkl?raw=true'))
+  # Load saved model pipeline
+  clf_pipeline = pickle.load(urlopen('https://github.com/mart-anthony-stark/Machine-Learning-and-Deep-Learning/blob/main/ph_fake_news/ph_fake_news_pipeline.pkl?raw=true'))
 
-# Target Classes
-mapped_dict = {'real': 0, 'fake': 1}
-mapped_list = list(mapped_dict.keys())
+  # Target Classes
+  mapped_dict = {'real': 0, 'fake': 1}
+  mapped_list = list(mapped_dict.keys())
 
-@app.route('/', methods=['GET','POST'])
-def index():
-  if request.method == 'POST':
-    data = request.json['news']
-    processed_data = preprocess_data(data)
+  @app.route('/', methods=['GET','POST'])
+  def index():
+    if request.method == 'POST':
+      data = request.json['news']
+      processed_data = preprocess_data(data)
 
-    data_arr = np.array([processed_data])
+      data_arr = np.array([processed_data])
 
-    prediction = clf_pipeline.predict(data_arr)
-    probability = clf_pipeline.predict_proba(data_arr)[0] * 100
-    predict_output = mapped_list[list(prediction)[0]]
+      prediction = clf_pipeline.predict(data_arr)
+      probability = clf_pipeline.predict_proba(data_arr)[0] * 100
+      predict_output = mapped_list[list(prediction)[0]]
 
-    return jsonify(data=data,
-                  processed_data=processed_data,
-                  is_fake=int(prediction[0]),
-                  real_probability=float(probability[0]),
-                  fake_probability=float(probability[1]),
-                  prediction_output=predict_output
-                  )
-  else:
-    data = preprocess_data("I've tested all suggested methods plus np.array(list(map(f, x))) with ... Most of the functions you'd want to apply to a NumPy array")
-    return data
+      return jsonify(data=data,
+                    processed_data=processed_data,
+                    is_fake=int(prediction[0]),
+                    real_probability=float(probability[0]),
+                    fake_probability=float(probability[1]),
+                    prediction_output=predict_output
+                    )
+    else:
+      data = preprocess_data("I've tested all suggested methods plus np.array(list(map(f, x))) with ... Most of the functions you'd want to apply to a NumPy array")
+      return data
 
-if __name__ == '__main__':
-   app.run()
+  if __name__ == '__main__':
+    app.run()
+
+run()
